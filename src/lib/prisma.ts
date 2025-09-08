ab1472ff54d3eb711f630aca9a1dab3662de31a9
akbar <fardeenakbar29@gmail.com>
2025-09-08 21:12:48 +0530

    Modified the prisma client to log queries and warnings in development only and log only errors in production

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -9,7 +9,10 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query"],
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"], // only log errors in production
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
